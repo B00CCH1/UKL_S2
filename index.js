916534a7d8e70929e9571bb6436b00a21d121c94
B00CCH1 <evan.sutanto12022009@gmail.com>
2026-06-02 23:32:58 +0700

    cors 1

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
